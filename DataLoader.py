09d472353d7b98f3f94992167b568128d79e89bc
Iason Sklavenitis <sklavenitisi6@gmail.com>
2021-06-21 18:13:14 +0300

    clean addresses for most cases

--- DataLoader.py
+++ DataLoader.py
@@ -37,29 +37,52 @@ class DataTransformer(object):
 
     @staticmethod
     def transform_addresses(addresses):
-        clean_address_list = []
+        clean_address_string_list = []
+        print(len(addresses))
         for address in addresses:
+            # Messy, inconsistent data rows
+            # TODO check double post codes
+            address = re.sub(',\\s+[A-Z\\d\\s]{3,}$', '', address)
             if 'https://' in address:
                 address_truncated = re.sub('\\shttps.*$', '', address)
-                clean_address_list.append(address_truncated.split(":", 1)[1])
+                clean_address_string_list.append(address_truncated.split(":", 1)[1].strip().rstrip('.'))
+            # todo check again
             elif 'This post will be split across practice and community' in address:
-                clean_address_list.append(DataTransformer.clean_split_practice(address))
+                clean_address_string_list.append(DataTransformer.clean_split_practice(address).rstrip('.'))
             elif 'N/A' in address:
-                clean_address_list.append('')
-            else:
-                # run to check names
-                #clean_address_list.append(address)
-                #print(address)
-                if 'Dent' or 'Clinic' or 'Practice' in address:
-                    clean_address_list.append(address)
-                elif '-' in address:
-                    clean_address_list.append(address.split("-", 1)[1])
+                clean_address_string_list.append('')
+            elif DataTransformer.check_dent_exists_in_substring(address):
+                if ',' not in address:
+                    # TODO check if spaces between words (capitals) is needed e.g. Billingshurst Dental Practice114 High StreetBillingshurstRH14 9QS
+                    clean_address_string_list.append(DataTransformer.truncate_hee_thames(address).rstrip('.'))
                 else:
-                    clean_address_list.append(address.split(",", 1)[1])
+                    comma_split_address = address.upper().strip().split(",")
+                    # Always for first result in list
+                    start_substring_index = [comma_split_address.index(i) for i in comma_split_address if
+                                             "DENT" in i or "PRACTICE" in i or "SMIL" in i][0]
+                    if '-' in comma_split_address[start_substring_index]:
+                        current_work_string = comma_split_address[start_substring_index]
+                        dash_index = current_work_string.index('-')
+                        current_work_string = current_work_string[dash_index + 1:]
+                        comma_split_address[start_substring_index] = current_work_string
+                        clean_address_string_list.append(
+                            ', '.join(comma_split_address[start_substring_index:]).strip().rstrip('.'))
+                    elif start_substring_index > 0:
+                        current_work_string = comma_split_address[start_substring_index]
+                        comma_index = current_work_string.index(',')
+                        current_work_string = current_work_string[comma_index + 1:]
+                        comma_split_address[start_substring_index] = current_work_string
+                        clean_address_string_list.append(
+                            ', '.join(comma_split_address[start_substring_index:]).strip().rstrip('.'))
 
+            elif '-' in address.split(",")[0] and ',' in address:
+                clean_address_string_list.append((address.split("-", 1)[1]).strip().rstrip('.'))
+            elif 'HEE Thames Valley and Wessex' in address:
+                clean_address_string_list.append(DataTransformer.truncate_hee_thames(address).rstrip('.'))
+            else:
+                clean_address_string_list.append(address)
 
-
-        return clean_address_list
+        return clean_address_string_list
 
     @staticmethod
     def clean_split_practice(address):
@@ -69,12 +92,17 @@ class DataTransformer(object):
             m = re.search(r"([A-Za-z0-9,\s]+)", address)
             return m.group(1)
 
-    "Dr Jane Williams - Cerrigcochion Road, Brecon, Ld3 7NS"
-    "  https://heeoe.hee.nhs.uk/node/8433   [Derbyshire Scheme]"
-    "Matford Dental Clinic, 1A The Venture Centre, Yeoford Way, Marsh Barton Business Centre, Exeter"
-    "Gurjit Moore, :  oraco dental PELSALL: Walsall, West Midlands. https://heeoe.hee.nhs.uk/node/8289"
-    "Banbury Dental Practice 35 High Street Banbury OX16 5ER - HEE Thames Valley and Wessex"
-    "Parkfield Dengh placement where you will spend 12 months in practice as a FD and then 12 months in a hospital post as a DCT at Taunton Hospital."
-    "N/A POST"
+    @staticmethod
+    def check_dent_exists_in_substring(address):
+        comma_separated_address = address.upper().split(",")
+        for i in comma_separated_address:
+            if ("DENT" in i) or ("PRACTICE" in i) or ("SMIL" in i):
+                return True
+        return False
 
-
+    @staticmethod
+    def truncate_hee_thames(address):
+        substring = 'HEE Thames Valley and Wessex'
+        if substring in address:
+            return re.sub('-\\s+' + substring, '', address)
+        return address
