18e14b01064f0ca19ab097dc8750d92d16b4c0aa
ZeenaShawa <zeena.shawa@kcl.ac.uk>
2021-06-21 09:55:36 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# DFTMapping Script
+# Authors: Zeena Shawa, Jason Sklavenitis
+
+# how to add instance of package to pycharm: https://stackoverflow.com/questions/35623776/import-numpy-on-pycharm
+import numpy as np
+import pandas as pd
+
+# load in data
+data = pd.read_csv('D:\PyCharm\DFTMapping\data\Preferences.csv')
+print(data['Description'])
+
+# def MappingInfo(address, ):
+
+# Determine whether address column is correctly formatted version or not
+for row in data.row :
+    if ':' in data.iloc[row]['Description'] :
+        print(True)
+
+# extract address based on case
+
+# find nearest_city
+
+# retrieve dist_nearest_city
+
+# retrieve star_rating
+
+# retrieve num_reviews
+
+# retrieve website (what our guess is for the website)
+
+
+# return nearest_city, dist_nearest_city, star_rating, num_reviews, website
+
