0da55709977bd23c0e73a2ea415e8127b6a0d2f2
ZeenaShawa <zeena.shawa@kcl.ac.uk>
2021-06-25 15:29:42 +0100

    finally ratings bit working

--- GoogleAPIConnector.py
+++ GoogleAPIConnector.py
@@ -1,4 +1,5 @@
 from configparser import SafeConfigParser
+
 import googlemaps
 
 
@@ -14,10 +15,18 @@ class GoogleAPIHttpClient(object):
         self.gmaps = googlemaps.Client(key=config['googlemaps']['key'], queries_per_second=100)
         pass
 
-
     def setup_places_api(self):
         self.url = 'maps.googleapis.com'
         self.path = '/maps/api/place/findplacefromtext/json?'
+
+    def setup_places_autocomplete_api(self):
+        self.url = 'maps.googleapis.com'
+        self.path = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?'
+
+    def send_autocomplete_dummy_test(self, address):
+        candidates_dict = self.gmaps.places_autocomplete_query(address, 'textquery', fields=["types", "establishment"])
+        print(candidates_dict)
+        return self.gmaps.place(candidates_dict['candidates'][0]['place_id'])
 
     def send_dummy_test(self, address):
         candidates_dict = self.gmaps.find_place(address, 'textquery', fields=None)
@@ -30,7 +39,6 @@ class GoogleAPIHttpClient(object):
         row_split = []
         row_count = 1
         for address in address_list:
-            print(row_count)
             if address == '':
                 address_uuid_list.append('N/A')
                 continue
@@ -47,24 +55,36 @@ class GoogleAPIHttpClient(object):
     # TODO merge row_split into one result back
     def find_address_uuid_from_list(self, address_uuid_list, row_split):
         address_list_details = []
-        for row_count in range(0, len(address_uuid_list)):
+        for row_count in range(0, len(address_uuid_list) - 1):
+            print(row_count)
             index_address = address_uuid_list[row_count]
-            index_address_minus_one = address_uuid_list[row_count-1]
+
             if index_address == 'N/A':
                 address_list_details.append("N/A")
             elif len(index_address['candidates']) == 0:
-                print(f"Error, fou  nd candidate list with 0 results at row: {row_count}")
+                print(f"Error, found candidate list with 0 results at row: {row_count}")
                 address_list_details.append("NULL_CANDIDATES")
             elif row_count in row_split:
-                joint_address_list = [self.gmaps.place(index_address['candidates'][0]['place_id']),
-                                      self.gmaps.place(index_address_minus_one['candidates'][0]['place_id'])]
+                index_address_plus_one = address_uuid_list[row_count + 1]
+                joint_address_list = [self.find_place(index_address),
+                                      self.find_place(index_address_plus_one)]
                 address_list_details.append(joint_address_list)
-                row_split.pop(row_split.index(row_count)) # OwO what is this?
+                row_split.pop(row_split.index(row_count))  # OwO what is this?
                 row_count += 1
             else:
                 if len(index_address['candidates']) > 1:
                     print(f"Warning, found candidate list with more than 1 result: {index_address}, "
                           f"using first result for row: {row_count}")
-                address_list_details.append(self.gmaps.place(index_address['candidates'][0]['place_id']))
+                address_list_details.append(self.find_place(index_address))
 
         return address_list_details
+
+    def find_place(self, address):
+        place = self.gmaps.place(address['candidates'][0]['place_id'])
+        if 'business_status' in place['result']:
+            return place
+        else:
+            test = self.gmaps.places_nearby(location=place['result']['geometry']['location'],
+                                                  radius=100,
+                                                  type='dentist')  # try at
+            return test
